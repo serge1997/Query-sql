c84d23a22ad473ce098a57e3e03dad3934f2c657
Lotchi Serge Gogo <serge.gogolo12@gmail.com>
2023-04-27 16:23:59 -0300

    Update SQL_Skills.sql

--- SQL_Skills.sql
+++ SQL_Skills.sql
@@ -3,3 +3,8 @@
   SELECT * FROM SB1010 WHERE B1_COD NOT IN (
     SELECT COD_PRODUTO FROM VENDAS 
     ) 
+  
+  -- Procururar informações sobre um produto especifico a partir do codigo de produto dentro da tabela de venda para a tabela dim de produto
+  SELECT * FROM VENDAS WHERE COD_PRODUTO IN (
+	    SELECT B1_COD FROM SB1010 WHERE B1_COD = '30225495'
+    )
