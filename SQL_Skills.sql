0e3101d9d195f444c7dd6cc55813fc40f1114cfe
Lotchi Serge Gogo <serge.gogolo12@gmail.com>
2023-04-28 11:47:24 -0300

    Update SQL_Skills.sql

--- SQL_Skills.sql
+++ SQL_Skills.sql
@@ -8,3 +8,10 @@
   SELECT * FROM VENDAS WHERE COD_PRODUTO IN (
 	    SELECT B1_COD FROM SB1010 WHERE B1_COD = '30225495'
     )
+    
+    
+--Cliente cadastrado que nunca fez uma compra uma compra. 
+--SA1010 Tabela de cliente, VENDAS tabela de venda
+SELECT * FROM SA1010 AS S WHERE NOT EXISTS(
+	SELECT * FROM VENDAS AS V WHERE V.COD_CLIENTE = S.A1_COD
+)
