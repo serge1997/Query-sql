801c09000278dbd343908d67e66fa15083a88c99
Lotchi Serge Gogo <serge.gogolo12@gmail.com>
2023-08-10 12:02:36 -0300

    Update CreateProcedure.sql

--- CreateProcedure.sql
+++ CreateProcedure.sql
@@ -4,3 +4,26 @@ CREATE PROCEDURE ProdVendas AS
 SELECT * FROM VENDAS WHERE CAST(EMISSAO AS DATE) = @data;
 
 EXECUTE sp_executesql ProdVendas;
+
+
+-- bloco de descições em PROCEDURE
+ALTER PROCEDURE infoCustomer 
+	@codCustomer VARCHAR(8), 
+	@emissao VARCHAR(8)
+AS
+	IF @codCustomer IS NULL OR @emissao IS NULL AND @codCustomer = '' OR @emissao = ''
+		BEGIN
+			PRINT 'Prenche os parametros'
+			RETURN
+		END
+	ELSE
+		BEGIN
+			SELECT * 
+			FROM VENDAS
+			WHERE
+				COD_CLIENTE = @codCustomer
+				AND EMISSAO = @emissao
+		END
+
+
+EXEC infoCustomer @codCustomer = null , @emissao = ''
